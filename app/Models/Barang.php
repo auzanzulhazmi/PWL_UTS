fe26b81dce7ce7b7f5f2e49994e1b12656f3edf7
auzanzulhazmi <zulhazmiauzan@gmail.com>
2021-03-30 09:58:02 +0700

    Membuat model barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Barang as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Barang extends Model
 {
-    use HasFactory;
+    protected $table ="barang"; // Eloquent membuat model barang menyimpan record di tabel barang
+    public $timestamps = false;
+    protected $primaryKey = 'id'; // Memanggil isi DB Dengan primarykey
+    /**
+    * The attributes that are mass assignable.
+    * 
+    * @var array
+    */
+    protected $fillable = [
+        'Id',
+        'Kode',
+        'Nama',
+        'Kategori',
+        'Harga',
+        'Qty',
+    ];
 }
