8478199c2c497f25c6dd4056617973cbff497e18
auzanzulhazmi <zulhazmiauzan@gmail.com>
2021-03-30 09:57:21 +0700

    Edit route barang

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BarangController;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('barang', BarangController::class);
